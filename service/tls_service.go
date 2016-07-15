756268227ef8386146adf2b14fe13c203df898fb
Tom Bowden <tom.b1992@gmail.com>
2016-07-15 16:41:59 +0100

    Attempt to parse certificates

--- service/tls_service.go
+++ service/tls_service.go
@@ -2,16 +2,33 @@ package service
 
 import (
   "crypto/tls"
+  "fmt"
 )
 
 type TLSService struct {
-  Service
+  name string
+  port int
+  dnsName string
   certificate tls.Certificate
   forceTLS bool
 }
 
+func (s TLSService) DNSName() string {
+  return s.dnsName
+}
+
+func (s TLSService) URL() string {
+	return fmt.Sprintf("http://%s:%d", s.name, s.port)
+
+}
+
 func NewTLSService(name string, port int, dnsName string, certificate string, key string) TLSService {
 
-  return TLSService{}
+  parsedCert, err := tls.X509KeyPair([]byte(certificate), []byte(key))
+
+  if err != nil {
+    panic(err)
+  }
+  return TLSService{name: name, port: port, dnsName: dnsName, certificate: parsedCert}
 
 }

cee5edd671478c9cedb21c3c0452df7d5d038bc0
Tom Bowden <tom.b1992@gmail.com>
2016-07-18 13:15:00 +0100

    Add better router testing

--- router/router_test.go
+++ router/router_test.go
@@ -1,125 +1,187 @@
 package router
 
 import (
-	"crypto/tls"
+	"io/ioutil"
 	"net/http/httputil"
 	"testing"
+
+	"github.com/tpbowden/swarm-ingress-router/service"
 )
 
-type TestService struct {
-	dnsName  string
-	url      string
-	forceTLS bool
+var certificate, certErr = ioutil.ReadFile("../fixtures/cert.crt")
+var key, keyErr = ioutil.ReadFile("../fixtures/key.key")
+
+type RouterTest struct {
+	description string
+	services    []service.Service
+	host        string
+	secure      bool
+	success     bool
+	redirect    bool
+	proxy       bool
 }
 
-func (t TestService) ForceTLS() bool {
-	return t.forceTLS
+var routerTests = []RouterTest{
+	RouterTest{
+		description: "A valid service returns an HTTP proxy",
+		host:        "example.local",
+		success:     true,
+		proxy:       true,
+		services: []service.Service{
+			service.Service{
+				Name:     "my-service",
+				Port:     3000,
+				DNSName:  "example.local",
+				Secure:   false,
+				ForceTLS: false,
+			},
+		},
+	},
+	RouterTest{
+		description: "A secure connection to an insecure service is not successful",
+		host:        "example.local",
+		success:     false,
+		secure:      true,
+		services: []service.Service{
+			service.Service{
+				Name:    "my-service",
+				Port:    3000,
+				DNSName: "example.local",
+				Secure:  false,
+			},
+		},
+	},
+	RouterTest{
+		description: "A missing service does not return successfully",
+		host:        "example.local",
+		services:    []service.Service{},
+	},
+	RouterTest{
+		description: "A service with an invalid URL does not return successfully",
+		host:        "example.local",
+		services: []service.Service{
+			service.Service{
+				Name:    "[::1]a",
+				Port:    3000,
+				DNSName: "example.local",
+			},
+		},
+	},
+	RouterTest{
+		description: "An insecure connection with forceTLS returns a redirect",
+		host:        "example.local",
+		success:     true,
+		redirect:    true,
+		services: []service.Service{
+			service.Service{
+				Name:     "my-service",
+				Port:     3000,
+				DNSName:  "example.local",
+				ForceTLS: true,
+			},
+		},
+	},
 }
 
-func (t TestService) DNSName() string {
-	return t.dnsName
-}
+func TestRouting(t *testing.T) {
+	for _, test := range routerTests {
+		subject := NewRouter()
+		subject.UpdateTable(test.services)
 
-func (t TestService) URL() string {
-	return t.url
-}
+		result, ok := subject.RouteToService(test.host, test.secure)
 
-func (t TestService) Certificate() (*tls.Certificate, bool) {
-	return &tls.Certificate{}, true
-}
+		if ok != test.success {
+			t.Errorf("Test failed: service fetching did not match: %s", test.description)
+		}
 
-func TestAddingARoute(t *testing.T) {
-	r := NewRouter()
-	service := Routable(TestService{dnsName: "www.example.com", url: "http://route.local"})
-	services := make([]Routable, 1)
-	services[0] = service
-	r.UpdateTable(services)
+		if test.redirect {
+			_, assertOk := result.(*RedirectHandler)
+			if !assertOk {
+				t.Errorf("Test failed: expected a redirect: %s", test.description)
+			}
+		}
 
-	actual, ok := r.RouteToService("www.example.com", true)
-
-	if !ok {
-		t.Error("Failed to lookup %www.example.com")
-	}
-
-	_, castOk := actual.(*httputil.ReverseProxy)
-
-	if !castOk {
-		t.Error("Expected result to be a reverse proxy to the service")
-	}
-}
-
-func TestRedirectingToHTTPS(t *testing.T) {
-	r := NewRouter()
-	service := Routable(TestService{dnsName: "www.example.com", forceTLS: true, url: "http://route.local"})
-	services := make([]Routable, 1)
-	services[0] = service
-	r.UpdateTable(services)
-
-	actual, ok := r.RouteToService("www.example.com", false)
-
-	if !ok {
-		t.Error("Failed to lookup %www.example.com")
-	}
-
-	_, castOk := actual.(*RedirectHandler)
-
-	if !castOk {
-		t.Error("Expected result to be a reverse proxy to the service")
-	}
-}
-
-func TestRoutingToInvalidMissing(t *testing.T) {
-	r := NewRouter()
-	service := Routable(TestService{dnsName: "", url: ""})
-	services := make([]Routable, 1)
-	services[0] = service
-	r.UpdateTable(services)
-
-	_, ok := r.RouteToService("www.nowhere.com", true)
-
-	if ok {
-		t.Error("Expected to fail to route to non-existant service")
-	}
-}
-
-func TestRoutingToInvalidService(t *testing.T) {
-	r := NewRouter()
-	service := Routable(TestService{dnsName: "www.nowhere.com", url: "http://[::1]a"})
-	services := make([]Routable, 1)
-	services[0] = service
-	r.UpdateTable(services)
-
-	_, ok := r.RouteToService("www.nowhere.com", true)
-
-	if ok {
-		t.Error("Expected to fail to route to invalid service")
-	}
-}
-
-func TestGettingACertificate(t *testing.T) {
-	r := NewRouter()
-	service := Routable(TestService{dnsName: "www.nowhere.com", url: "http://[::1]a"})
-	services := make([]Routable, 1)
-	services[0] = service
-	r.UpdateTable(services)
-
-	_, ok := r.CertificateForService("www.nowhere.com")
-
-	if !ok {
-		t.Error("Failed to retrieve certificate for service")
+		if test.proxy {
+			_, assertOk := result.(*httputil.ReverseProxy)
+			if !assertOk {
+				t.Errorf("Test failed: expected a reverse proxy: %s", test.description)
+			}
+		}
 	}
 }
 
-func TestGettingACertificateForMissingService(t *testing.T) {
-	r := NewRouter()
-	service := Routable(TestService{dnsName: "www.nowhere.com", url: "http://[::1]a"})
-	services := make([]Routable, 1)
-	services[0] = service
-	r.UpdateTable(services)
+type CertificateTest struct {
+	description string
+	services    []service.Service
+	host        string
+	success     bool
+}
 
-	_, ok := r.CertificateForService("www.wrong.com")
+var certificateTests = []CertificateTest{
+	CertificateTest{
+		description: "Missing services do not return successfully",
+		services: []service.Service{
+			service.Service{
+				Name:    "my-service",
+				Port:    3000,
+				DNSName: "example.local",
+				Secure:  false,
+			},
+		},
+		host:    "foo.local",
+		success: false,
+	},
+	CertificateTest{
+		description: "Invalid certificates do not return successfully",
+		services: []service.Service{
+			service.Service{
+				Name:        "my-service",
+				Port:        3000,
+				DNSName:     "example.local",
+				Secure:      true,
+				EncodedCert: "some data",
+				EncodedKey:  "some data",
+			},
+		},
+		host:    "example.local",
+		success: false,
+	},
+	CertificateTest{
+		description: "Valid certificates return successfully",
+		services: []service.Service{
+			service.Service{
+				Name:        "my-service",
+				Port:        3000,
+				DNSName:     "example.local",
+				Secure:      true,
+				EncodedCert: string(certificate),
+				EncodedKey:  string(key),
+			},
+		},
+		host:    "example.local",
+		success: true,
+	},
+}
 
-	if ok {
-		t.Error("Expected to fail to retrieve missing certificate")
+func TestCertificates(t *testing.T) {
+
+	if certErr != nil {
+		t.Error("Failed to read certificate fixture: ", certErr)
+	}
+
+	if keyErr != nil {
+		t.Error("Failed to read key fixture: ", keyErr)
+	}
+
+	for _, test := range certificateTests {
+		subject := NewRouter()
+		subject.UpdateTable(test.services)
+
+		_, ok := subject.CertificateForService(test.host)
+
+		if ok != test.success {
+			t.Errorf("Test failed: certificate fetching did not match: %s", test.description)
+		}
+
 	}
 }
